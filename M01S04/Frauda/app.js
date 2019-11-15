9615d41bc126d8c7e2c1134022423ef22472425e
Lica <48829574+EMKAcademy@users.noreply.github.com>
2019-11-15 19:58:18 +0200

    sarrrrrrrrpiliiiiiiii

---
+++ M01S04/Frauda/app.js
@@ -0,0 +1,9 @@
+var watcherElement = document.getElementById('watcher-count');
+watchers =  Math.floor(Math.random() * 10) + 1;
+
+watcherElement.innerText = watchers;
+
+var timer = setInterval(function(){
+    watchers =  Math.floor(Math.random() * 10) + 1;
+    watcherElement.innerText = watchers;
+},1000)
